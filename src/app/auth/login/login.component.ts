d3d1591409f9aa3d3845b6642af8da381802b09b
Felipe <felipemph2@gmail.com>
2022-01-20 14:23:27 -0300

    Implementação do AuthService, Integração parcial com componentes, e melhorias no header

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
+import { AuthService } from './../../core/services/auth/auth.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +13,39 @@ export class LoginComponent implements OnInit {
   @ViewChild('login') login!: NgForm; //O segundo login pode ser qualquer coisa. Inclusive loginForm para diferenciar
 
   onSubmit(){
-    console.log(this.login.value);
+    const email = this.login.value.email as string;
+    const password = this.login.value.password as string;
+
+    this.authService.login(email, password).subscribe({
+      next: (creds) =>{
+
+      },
+      error: (err) =>{
+        let message = "Ocorreu um erro";
+
+        switch(err.code){
+          case "auth/invalid-email":
+            message = "Email inválido";
+            break;
+          case "auth/user-not-found":
+            message = "Usuário não encontrado";
+            break;
+        }
+
+        this.snackBar.open(message, 'Fechar', {
+          duration: 5000,
+          horizontalPosition: 'end'
+        })
+      }
+    }
+
+    )
   }
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+    ) { }
 
   ngOnInit(): void {
   }
