271b8fb45bcabf37767dd71e0362e3157f50eac7
Felipe <felipemph2@gmail.com>
2022-01-20 18:10:20 -0300

    Edição de perfil e upload de imagem de usuário

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,53 +1,51 @@
-import { AuthService } from './../../core/services/auth/auth.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('login') loginForm!: NgForm;
 
-  @ViewChild('login') login!: NgForm; //O segundo login pode ser qualquer coisa. Inclusive loginForm para diferenciar
-
-  onSubmit(){
-    const email = this.login.value.email as string;
-    const password = this.login.value.password as string;
+  onSubmit() {
+    const email = this.loginForm.value.email as string;
+    const password = this.loginForm.value.password as string;
 
     this.authService.login(email, password).subscribe({
-      next: (creds) =>{
-
+      next: (creds) => {
+        // TODO: Verificar o bug do clique
+        console.log("foi")
       },
-      error: (err) =>{
-        let message = "Ocorreu um erro";
+      error: (err) => {
+        let message = 'Ocorreu um erro';
 
-        switch(err.code){
-          case "auth/invalid-email":
-            message = "Email inválido";
+        switch (err.code) {
+          case 'auth/invalid-email':
+            message = 'Email inválido';
             break;
-          case "auth/user-not-found":
-            message = "Usuário não encontrado";
+          case 'auth/user-not-found':
+            message = 'Usuário não encontrado';
             break;
         }
 
         this.snackBar.open(message, 'Fechar', {
           duration: 5000,
-          horizontalPosition: 'end'
-        })
-      }
-    }
-
-    )
+          horizontalPosition: 'end',
+        });
+      },
+    });
   }
 
   constructor(
     private authService: AuthService,
-    private snackBar: MatSnackBar
-    ) { }
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
