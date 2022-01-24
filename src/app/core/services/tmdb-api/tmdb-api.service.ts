4f0fee4959c5fa12dbc22cfcfc7a6fa6997460e8
Felipe <felipemph2@gmail.com>
2022-01-24 18:09:25 -0300

    Criação de busca reativa e listagem de filmes e séries

--- src/app/core/services/tmdb-api/tmdb-api.service.ts
+++ src/app/core/services/tmdb-api/tmdb-api.service.ts
@@ -1,16 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { MovieTvBase } from '../../models/movie-tv-base';
+
+type ApiResponse = {page: number, results: MovieTvBase[]}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TmdbApiService {
 
-  baseUrl = 'https://api.themoviedb.org/3/';
+  baseUrl = 'https://api.themoviedb.org/3'
 
   options = {
     api_key: 'cca6175097ce75bf3b6fa679e974860d',
     language: 'pt-BR'
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  trending(): Observable<MovieTvBase[]>{
+    return this.http.get<ApiResponse>(`${this.baseUrl}/trending/all/week`, {
+      params: this.options
+    }).pipe(map(data => data.results)) //Irá mapear meu JSON trasendo apenas o que eu quero: data.results
+  }
+
+
+  search(query: string): Observable<MovieTvBase[]> {
+    return this.http.get<ApiResponse>(`${this.baseUrl}/search/multi`, { //dentro desse objeto tenho o nome da propriedade e seu valor
+      params: {
+        ...this.options, //desestruturo minha variável e coloco seu conteúdo dentro desse objeto params
+        include_adult: false, //informo para API que n quero trazer os filmes adultos
+        query: query //procura dentro do meu JSON todos os filmes que contem o parâmetro digitado pelo usuário
+      }
+    }).pipe(map((data => data.results)))
+  }
 }
